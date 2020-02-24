ea7ee4d5a267cad2181e6b19dcc6cb76fc26cae1
Qisen Lin <qslin1994@gmail.com>
2020-02-24 16:28:28 +0900

    fix spaces and tabs

--- true_ram_sclk.v
+++ true_ram_sclk.v
@@ -7,39 +7,39 @@
 
 module true_ram_sclk
 (
-	input [31:0] data,
-	input [17:0] wr_addr,rd_addr,
-	input wr_ena,  wr_clk, rd_ena, rd_clk,
-	output reg [31:0] q,
-	output reg dval
+    input [31:0] data,
+    input [17:0] wr_addr,rd_addr,
+    input wr_ena,  wr_clk, rd_ena, rd_clk,
+    output reg [31:0] q,
+    output reg dval
 );
-	// Declare the RAM variable
-	reg [31:0] ram[230000:0];
-	
-	// Write
-	always @ (posedge wr_clk)
-	begin
+    // Declare the RAM variable
+    reg [31:0] ram[230000:0];
+    
+    // Write
+    always @ (posedge wr_clk)
+    begin
       if (wr_ena) 
       begin
         ram[wr_addr] <= data;
-			//q_a <= data_a;
+        //q_a <= data_a;
       end
-	end
+    end
 
-	// Read	
-	always @ (posedge rd_clk)
-	begin
-	  if(rd_ena)
-	  begin
-	    q <= ram[rd_addr];
+    // Read 
+    always @ (posedge rd_clk)
+    begin
+      if(rd_ena)
+      begin
+        q <= ram[rd_addr];
         dval <= 1'b1;
-	  end
+      end
 
       else
-	    dval <= 1'b0;		
-		  
-		  //q_a <= 16'b0;
+        dval <= 1'b0;       
+          
+        //q_a <= 16'b0;
 
-	end
-	
-endmodule	
+    end
+    
+endmodule   
